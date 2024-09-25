da88db408c63de483f6d8bb6c6fbd23ee189f3f7
Baskar Shanmugam <test.sca.github@gmail.com>
2024-09-25 09:29:59 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/lestrrat-go/jwx/v2"
 )
 
 func main() {
+
+	_ = jwx.GuessFormat([]byte("{}"))
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
